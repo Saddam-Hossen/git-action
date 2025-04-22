1a1c55bc367776e444ee140c61d956df01d1b512
Saddam <01957987631a@gmail.com>
2025-04-22 11:53:00 +0600

    Test Code

--- src/main/java/com/example/git_action/GitActionApplication.java
+++ src/main/java/com/example/git_action/GitActionApplication.java
@@ -6,8 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitActionApplication {
 
+	public static void check() {
+		System.out.println("Hello Saddam. Are you okay?");
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(GitActionApplication.class, args);
+		check();
 	}
 
 }
