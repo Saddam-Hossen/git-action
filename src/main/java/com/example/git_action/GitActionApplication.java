be236e8074ac240996817ffea52ca403946d906e
Saddam <01957987631a@gmail.com>
2025-04-22 14:36:38 +0600

    Error Fixing For Deploya

--- src/main/java/com/example/git_action/GitActionApplication.java
+++ src/main/java/com/example/git_action/GitActionApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitActionApplication {
 
 	public static void check() {
-		System.out.println("Hello Saddam. Are you okay?");
+		System.out.println("Hello Saddam. Are you okay? Yes");
 	}
 
 	public static void main(String[] args) {
